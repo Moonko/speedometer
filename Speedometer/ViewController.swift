5fe432025d39ad6a5384062041deaa19fabaa1f3
Andrei Rychkov <rychkov.dev@gmail.com>
2022-08-08 17:14:26 +1000

    Initial Commit

---
+++ Speedometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speedometer
+//
+//  Created by Andrei Rychkov on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
